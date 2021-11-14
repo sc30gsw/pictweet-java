ff82afdbf9a92a0f59eb2bb50ee39ae02d266a39
sc30gsw <haidoushi875@gmail.com>
2021-11-14 17:43:04 +0900

    ユーザー登録サービスの作成
    UserのServiceクラスの作成

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.MUser;
+import com.example.demo.form.SignupForm;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public void setUser(SignupForm form) {
+		MUser user = new MUser();
+		user.setUsername(form.getUsername());
+		user.setEmail(form.getEmail());
+		user.setPassword(form.getPassword());
+		userRepository.save(user);
+	}
+
+}
