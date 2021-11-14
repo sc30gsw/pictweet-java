91fa738cc3106f4aeb57dc16e8d364294bc8eff5
sc30gsw <haidoushi875@gmail.com>
2021-11-14 17:44:06 +0900

    新規登録のフォームクラスの作成

---
+++ src/main/java/com/example/demo/form/SignupForm.java
@@ -0,0 +1,25 @@
+package com.example.demo.form;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+public class SignupForm {
+
+	@NotBlank
+	@Email
+	private String email;
+	
+	@NotBlank
+	@Size(min = 4, max = 64)
+	private String username;
+	
+	@NotBlank
+	@Size(min = 8, max = 128)
+	@Pattern(regexp = "^[A-Za-z0-9]+$")
+	private String password;
+}
