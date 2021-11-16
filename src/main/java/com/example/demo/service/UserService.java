d01e4561d84e05fc67a928c5dbdfc952f7964bb6
sc30gsw <haidoushi875@gmail.com>
2021-11-17 06:05:55 +0900

    パスワードの暗号化

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.MUser;
@@ -11,13 +12,18 @@ import com.example.demo.repository.UserRepository;
 public class UserService {
 	
 	@Autowired
+	private PasswordEncoder encoder;
+	
+	@Autowired
 	private UserRepository userRepository;
 	
 	public void setUser(SignupForm form) {
 		MUser user = new MUser();
 		user.setUsername(form.getUsername());
 		user.setEmail(form.getEmail());
-		user.setPassword(form.getPassword());
+		//パスワードの暗号化
+		String rowPassword = form.getPassword();
+		user.setPassword(encoder.encode(rowPassword));
 		user.setRole("ROLE_GENERAL");
 		userRepository.save(user);
 	}
