a3dfd89c3272e7fc8540b9e71fa71ab4e4dabe11
sc30gsw <haidoushi875@gmail.com>
2021-11-24 06:09:43 +0900

    投稿詳細サービスメソッドの作成

--- src/main/java/com/example/demo/service/TweetService.java
+++ src/main/java/com/example/demo/service/TweetService.java
@@ -1,10 +1,12 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.entity.MTweet;
 import com.example.demo.form.TweetForm;
@@ -18,6 +20,7 @@ public class TweetService {
 	private TweetRepository tweetRepository;
 	
 	/**投稿機能*/
+	@Transactional
 	public void setTweet(TweetForm form, @AuthenticationPrincipal SimpleLoginUser loginUser) {
 		MTweet tweet = new MTweet();
 		tweet.setUserId(loginUser.getUser().getUserId());
@@ -29,5 +32,13 @@ public class TweetService {
 	/**投稿全取得*/
 	public List<MTweet> findAllTweets(){
 		return tweetRepository.findAll();
+	}
+	
+	/**投稿取得(1件)*/
+	public MTweet getTweetOne(Integer tweetId) {
+		Optional<MTweet> option = tweetRepository.findById(tweetId);
+		MTweet tweet = option.orElse(null);
+		
+		return tweet;
 	}
 }
